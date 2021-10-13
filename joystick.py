ad7f9e275cf11e0edb31e90aa1293b6c78de9edc
vnathan1 <91503462+vnathan1@users.noreply.github.com>
2021-10-13 03:04:26 +0000

    a lot

--- joystick.py
+++ joystick.py
@@ -1,10 +1,11 @@
-import PCF8591 as PCF8591
+import PCF8591 as adc 
 
 class Joystick:
-  pass
+  def __init__(self,address):
+    self.value = adc.PCF8591(address)
+  def getX(self):
+    return self.value.read(2)
+  def getY(self):
+    return self.value.read(1)
 
-def getX(self):
-  return
-def getY(self):
-  return
 
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-re
+import joystick
+import time
+a = 1
+address = 0x48
+analog = joystick.Joystick(address)
+
+while a > 0 :
+  x_value = getX()
+  y_value = getY()
+  print(x_value + ', ' + y_value)
